344961945384de0ff325dae4bf03859d9ef2e3f7
Zdunking <zhouqi2617@qq.com>
2025-06-07 06:45:55 +0800

    little gift from brothers

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jkkk
 jkkk
+
+# author
+xiao7isacat@gmail.com
+Zdunking
+
+# Usage
+
+python test.py
+
+visit 127.0.0.1:8000 and enjoy it
--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 print("Hello, World!")
+
+# http 静态服务器
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+class RedirectHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if not os.path.exists(self.translate_path(self.path)) or os.path.isdir(self.translate_path(self.path)):
+            self.path = '/train.html'
+        return super().do_GET()
+def main():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RedirectHandler)
+    # 挂载当前目录
+    
+    # 重定向到/train.html
+    
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
